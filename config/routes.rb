ef3600ce20f494d17ae5588acb8fc0a421329a09
idzzy <idzzyzzbi@gmail.com>
2016-02-21 07:09:34 +0000

    add sign sign up and user detail

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   # get 'static_pages/home'
+  #get 'users/new'
+
   root to: 'static_pages#home'
-  
+  get 'signup',  to: 'users#new'
+
+  resources :users
+
 end
